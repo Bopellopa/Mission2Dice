543e4da086134f92dcdcc4929309b8b5c9ea2cbc
bopellopa <bellak20@byu.edu>
2023-01-19 00:02:28 -0700

    first change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace Mission2Dice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rolls;
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many dice rolls would you like to simulate?:");
+            rolls = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS \n Each '*' represents 1 % of the total number of rolls. \n Total number of rolls = " + rolls);
+            int[] dicecombo;
         }
     }
 }
